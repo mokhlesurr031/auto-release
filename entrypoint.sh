ffac432545566bff8c36454dba3bf3d8cb7ac914
Mokhlesur Mahin <mokhlesurr031@gmail.com>
2023-12-11 23:37:11 +0600

    all

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-echo "Minor $MINOR_MAX_COUNT"
-echo "Hello $RELEASE_TYPE"
+echo "Minor $INPUT_MINOR_MAX_COUNT"
+echo "Hello $INPUT_RELEASE_TYPE"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 cat $GITHUB_OUTPUT
